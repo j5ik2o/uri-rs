0103a910b07334bb41e60e026a81346ceb9d4298
Junichi Kato <j5ik2o@gmail.com>
2021-09-01 00:00:21 +0900

    wip

--- benches/bench_main.rs
+++ benches/bench_main.rs
@@ -3,7 +3,8 @@
 #![allow(dead_code)]
 
 use criterion::*;
-use uri_parsing_rs::parser::parsers::{Elms};
+use uri_rs::parser;
+use uri_rs::parser::parsers::Elms;
 
 criterion_group!(benches, criterion_benchmark);
 
@@ -12,7 +13,7 @@ const uri: &'static str =
 
 #[inline]
 fn uri_parsers_uri() {
-  let _ = uri_parsing_rs::parser::parsers::uri_parsers::uri(Elms::new(uri.as_bytes())).unwrap();
+  let _ = parser::parsers::uri_parsers::uri(Elms::new(uri.as_bytes())).unwrap();
 }
 
 const hier_part: &'static str = "//user1:pass1@localhost:80801";
@@ -20,13 +21,13 @@ const hier_part: &'static str = "//user1:pass1@localhost:80801";
 #[inline]
 fn uri_hier_part_parsers_hier_part() {
   let _ =
-      uri_parsing_rs::parser::parsers::hier_part_parsers::hier_part(Elms::new(hier_part.as_bytes())).unwrap();
+      parser::parsers::hier_part_parsers::hier_part(Elms::new(hier_part.as_bytes())).unwrap();
 }
 
 const query: &'static str = "key1=value1&key2=value2&key1=value2";
 
 fn uri_query_parsers_query() {
-  let _ = uri_parsing_rs::parser::parsers::query_parsers::query(Elms::new(query.as_bytes())).unwrap();
+  let _ = parser::parsers::query_parsers::query(Elms::new(query.as_bytes())).unwrap();
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
